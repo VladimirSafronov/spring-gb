3c340c917f444cdc212a7b81250927a2b0283ffe
Vladimir <89627108+VladimirSafronov@users.noreply.github.com>
2024-02-04 22:22:42 +0600

    Delete spring-security-hw directory

--- spring-security-hw/src/main/java/ru/safronov/springsecurityhw/helpers/Util.java
+++
@@ -1,7 +0,0 @@
-package ru.safronov.springsecurityhw.helpers;
-
-public class Util {
-
-  public static Long ID = 1L;
-
-}
--- spring-security-hw/src/main/java/ru/safronov/springsecurityhw/repositoy/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.safronov.springsecurityhw.repositoy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.safronov.springsecurityhw.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
